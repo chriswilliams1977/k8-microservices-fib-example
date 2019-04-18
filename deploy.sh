49ee651dabfa39306bea173e0da21d8de928cd49
Chris Williams <chriswilliams1977@gmail.com>
2019-04-18 10:15:11 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,18 @@ docker build -t chriswilliams1977/multi-server:latest -t chriswilliams1977/multi
 docker build -t chriswilliams1977/multi-worker:latest -t chriswilliams1977/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker tag chriswilliams1977/multi-client gcr.io/williamscj-gke-demos/multi-client:latest
-docker tag chriswilliams1977/multi-client gcr.io/williamscj-gke-demos/multi-server:latest
-docker tag chriswilliams1977/multi-client gcr.io/williamscj-gke-demos/multi-worker:latest
+docker tag chriswilliams1977/multi-server gcr.io/williamscj-gke-demos/multi-server:latest
+docker tag chriswilliams1977/multi-worker gcr.io/williamscj-gke-demos/multi-worker:latest
 docker tag chriswilliams1977/multi-client gcr.io/williamscj-gke-demos/multi-client:$SHA
-docker tag chriswilliams1977/multi-client gcr.io/williamscj-gke-demos/multi-server:$SHA
-docker tag chriswilliams1977/multi-client gcr.io/williamscj-gke-demos/multi-worker:$SHA
+docker tag chriswilliams1977/multi-server gcr.io/williamscj-gke-demos/multi-server:$SHA
+docker tag chriswilliams1977/multi-worker gcr.io/williamscj-gke-demos/multi-worker:$SHA
 
 docker push gcr.io/williamscj-gke-demos/multi-client:latest
 docker push gcr.io/williamscj-gke-demos/multi-server:latest
 docker push gcr.io/williamscj-gke-demos/multi-worker:latest
 docker push gcr.io/williamscj-gke-demos/multi-client:$SHA
-docker push gcr.io/williamscj-gke-demos/multi-server$SHA
-docker push gcr.io/williamscj-gke-demos/multi-worker$SHA
+docker push gcr.io/williamscj-gke-demos/multi-server:$SHA
+docker push gcr.io/williamscj-gke-demos/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=gcr.io/williamscj-gke-demos/multi-server:$SHA
